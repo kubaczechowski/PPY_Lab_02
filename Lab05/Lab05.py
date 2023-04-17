02d5d207782a142365d32bcc46706f777b8574f3
kubaczechowski <67064580+kubaczechowski@users.noreply.github.com>
2023-04-17 21:46:37 +0200

    lab 05 finished

--- Lab05/Lab05.py
+++ Lab05/Lab05.py
@@ -1,38 +1,63 @@
 filepath = "students2.txt"
-studentsDict = {}
+
+students = {}
 
 
-def add_grades_for_students_who_dont_have_them(students_dict):
-    for email in students_dict:
-        if email in students_dict and "grade" not in students_dict[email]:
-            #here is the problem...
-            students_dict[email] = {"grade": students_dict[email]["punkty"] < 75 if "GOOD" else "VERY GOOD"}
+def save_data():
+    with open('students.txt', 'w') as f:
+        for email, student_data in students.items():
+            line = f"{email},{student_data['name']},{student_data['surname']},{student_data['points']},{student_data['grade']},{student_data['status']}\n"
+            f.write(line)
+    print("Data has been saved to file.")
 
 
-with open(filepath) as file_object:
-    for line in file_object:
-        print(line.rstrip())
-        parts = line.strip().split(",")
-        if len(parts) == 4:
-            email, imie, nazwisko, punkty = parts
-            studentsDict[email] = {
-                "imie": imie,
-                "nazwisko": nazwisko,
-                "punkty": int(punkty),
-            }
-        elif len(parts) == 5:
-            email, imie, nazwisko, punkty, grade = parts
-            studentsDict[email] = {
-                "imie": imie,
-                "nazwisko": nazwisko,
-                "punkty": int(punkty),
-                "grade": grade
-            }
+with open(filepath, 'r') as f:
+    for line in f:
+        parts = line.strip().split(',')
+        email, name, surname, points = parts[:4]
+        grade = parts[4] if len(parts) >= 5 else ''
+        status = parts[5] if len(parts) >= 6 else ''
+        if email not in students:
+            students[email] = {'name': name, 'surname': surname, 'points': int(points), 'grade': grade,
+                               'status': status}
+        else:
+            students[email]['points'] += int(points)
 
-add_grades_for_students_who_dont_have_them(studentsDict)
+print(students)
 
-with open("students.txt", "w") as file:
-    for email, student in studentsDict.items():
-        print(student)
-        #line = f"{email},{student['imie']},{student['nazwisko']},{student['punkty']}\n"
-        file.write(line)
+for student in students.values():
+    if student['status'] not in ('GRADED', 'MAILED'):
+        if student['points'] >= 90:
+            student['grade'] = '5.0'
+        elif student['points'] >= 75:
+            student['grade'] = '4.5'
+        elif student['points'] >= 60:
+            student['grade'] = '4.0'
+        elif student['points'] >= 50:
+            student['grade'] = '3.5'
+        elif student['points'] >= 40:
+            student['grade'] = '3.0'
+        else:
+            student['grade'] = '2.0'
+        student['status'] = 'GRADED'
+    save_data()
+
+print(students)
+
+
+def add_student(email, name, surname, points):
+    if email in students:
+        print(f"Student with email {email} already exists!")
+    else:
+        students[email] = {'name': name, 'surname': surname, 'points': int(points), 'grade': '', 'status': ''}
+        print(f"Student {name} {surname} with email {email} and {points} points has been added.")
+        save_data()
+
+
+def remove_student(email):
+    if email in students:
+        del students[email]
+        print(f"Student with email {email} has been removed.")
+        save_data()
+    else:
+        print(f"Student with email {email} does not exist.")
